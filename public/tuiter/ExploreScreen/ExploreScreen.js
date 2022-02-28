a7324af1301df199fb15a3a635877f9d47a6fbd8
dsilva-d <dsilva.d@northeastern.edu>
2022-02-27 20:58:59 -0500

    navigation

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -8,10 +8,16 @@ import NavigationSidebar from "../NavigationSidebar.js";
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
             ${NavigationSidebar()}
         </div>
+
+
+
+
+
+        </div>
+    `);
+})($);
+
         //    ${ExploreComponent()}
         //<div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
         //    ${WhoToFollowList()}
         //</div>
-        </div>
-    `);
-})($);
--- public/tuiter/NavigationSidebar.js
+++ public/tuiter/NavigationSidebar.js
@@ -3,12 +3,27 @@ const NavigationSidebar = () => {
             <div class="list-group">
                 <a class="list-group-item" href="/">
                     <i class="fab fa-twitter"></i></a>
-            <!-- continue the rest of the list -->
+                <a class="list-group-item" href="./home.html">
+                                    <i class="fa fa-home"> Home</i></a>
+                <a class="list-group-item active" href="./explore.html">
+                                    <i class="fa fa-solid fa-hashtag"> Explore</i></a>
+                <a class="list-group-item" href="./notifications.html">
+                                    <i class="fa fa-solid fa-bell"> Notifications</i></a>
+                <a class="list-group-item" href="./messages.html">
+                                    <i class="fa fa-solid fa-envelope"> Messages</i></a>
+                <a class="list-group-item" href="./bookmarks.html">
+                                    <i class="fa fa-solid fa-bookmark"> Bookmarks</i></a>
+                <a class="list-group-item" href="./lists.html">
+                                    <i class="fa fa-solid fa-list"> Lists</i></a>
+                <a class="list-group-item" href="./profile.html">
+                                    <i class="fa fa-solid fa-user"> Profile</i></a>
+                <a class="list-group-item" href="./profile.html">
+                                    <i class="fa fa-ellipsis-h"> More</i></a>
             </div>
             <div class="d-grid mt-2">
-                <a href="tweet.html"
+                <a href="tuit.html"
                    class="btn btn-primary btn-block rounded-pill">
-                    Tweet</a>
+                    Tuit</a>
             </div>
     `);
 }
