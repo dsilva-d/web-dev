8032918e9b83645c2b466bdcb027a328a930caf3
dsilva-d <dsilva.d@northeastern.edu>
2022-02-27 19:19:38 -0500

    navigation

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,5 +1,5 @@
-//import NavigationSidebar from "../NavigationSidebar.js";
-//import ExploreComponent from "./ExploreComponent.js";
+import NavigationSidebar from "../NavigationSidebar.js";
+//import ExploreComponent from "../ExploreComponent.js";
 //import WhoToFollowList from "../WhoToFollowList.js";
 
 (function ($) {
@@ -8,10 +8,10 @@
         <div class="col-2 col-md-2 col-lg-1 col-xl-2">
             ${NavigationSidebar()}
         </div>
-            ${ExploreComponent()}
-        <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-            ${WhoToFollowList()}
-        </div>
+        //    ${ExploreComponent()}
+        //<div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+        //    ${WhoToFollowList()}
+        //</div>
         </div>
     `);
 })($);
