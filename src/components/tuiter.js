d8d8062f6705aacd5723e244457dd5b74b9b986e
dsilva-d <dsilva.d@northeastern.edu>
2022-03-13 22:23:38 -0400

    lab

---
+++ src/components/tuiter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Tuiter = () => {
+    return(
+    <ul>
+      <li><a href="tuiter/navigation.html">Tuiter</a></li>
+    </ul>
+    )
+};
+export default Tuiter;
