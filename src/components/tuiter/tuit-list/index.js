50a8b728f286d7b707bcdfc45e8f555ad02def29
dsilva-d <dsilva.d@northeastern.edu>
2022-04-06 02:03:04 -0400

    a8

--- src/components/tuiter/tuit-list/index.js
+++ src/components/tuiter/tuit-list/index.js
@@ -1,10 +1,20 @@
-import React from "react";
-import TuitListItem
-       from "./tuit-list-item";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import * as service from '../../services/tuits-service';
+import TuitListItem from "./tuit-list-item";
 
 const TuitList = () => {
   const tuits = useSelector(state => state.tuits);
+  const dispatch = useDispatch();
+  const findAllTuits = async () => {
+    const tuits = await service.findAllTuits();
+    dispatch({
+      type: 'FIND_ALL_TUITS',
+      tuits: tuits
+    });
+  }
+  useEffect(findAllTuits, []);
+
   return (
     <ul className="ttr-tuits list-group">
       {
